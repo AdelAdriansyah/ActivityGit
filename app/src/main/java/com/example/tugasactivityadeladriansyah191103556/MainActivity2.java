ae502e313ce1c2ae3c0ba202605de781266ee986
AdelAdriansyah <odeladriansyah80@gmail.com>
2022-04-21 04:55:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugasactivityadeladriansyah191103556/MainActivity2.java
@@ -0,0 +1,25 @@
+package com.example.tugasactivityadeladriansyah191103556;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    TextView txtHalo;
+    private String nama;
+    private String key_name = "NAMA";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        txtHalo = (TextView) findViewById(R.id.txtHalo);
+
+        Bundle extras = getIntent().getExtras();
+        nama = extras.getString(key_name);
+        txtHalo.setText("Halo " + nama);
+    }
+}
